e48b86b2c1c4787dde24cd0ddfd29c3f89c358df
koo <jhk5361@gmail.com>
2022-09-29 00:43:55 +0900

    Add an aio flag

--- src/platform/aio.c
+++ src/platform/aio.c
@@ -12,6 +12,7 @@ aio_read(struct handler *hlr, struct dev_abs *dev, uint64_t addr_in_byte, uint32
 
 	struct iocb *iocb = (struct iocb *)q_dequeue(cb->hlr->iocb_pool);
 	io_prep_pread(iocb, dev->dev_fd, buf, size, addr_in_byte);
+	iocb->aio_rw_flags = RWF_HIPRI;
 
 	cb->iocb = iocb;
 	iocb->data = cb;
@@ -37,6 +38,7 @@ aio_write(struct handler *hlr, struct dev_abs *dev, uint64_t addr_in_byte, uint3
 
 	struct iocb *iocb = (struct iocb *)q_dequeue(cb->hlr->iocb_pool);
 	io_prep_pwrite(iocb, dev->dev_fd, buf, size, addr_in_byte);
+	iocb->aio_rw_flags = RWF_HIPRI;
 
 	cb->iocb = iocb;
 	iocb->data = cb;
